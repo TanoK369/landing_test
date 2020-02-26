ca9cde5285ca81c3b217ed4a868ab177a2a9c295
Ivan <raikov.io@gmail.com>
2020-02-26 19:22:58 +0300

    Init

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
